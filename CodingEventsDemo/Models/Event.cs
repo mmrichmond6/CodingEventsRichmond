fbea3f07fcd6640dc89b76cfdea5e70d93a97e91
mmrichmond6 <mmrichmond6@gmail.com>
2022-10-06 17:33:38 -0500

    Name & Description finished

---
+++ CodingEventsDemo/Models/Event.cs
@@ -0,0 +1,19 @@
+﻿namespace CodingEventsDemo.Models
+{
+    public class Event
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public Event(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
