d80531efaa0d826c902021e9297ca1e4788c3bd7
mmrichmond6 <mmrichmond6@gmail.com>
2022-11-06 08:47:35 -0600

    Added EventCategory

---
+++ CodingEventsDemo/Models/EventCategory.cs
@@ -0,0 +1,17 @@
+﻿namespace CodingEventsDemo.Models
+{
+    public class EventCategory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public EventCategory()
+        {
+
+        }
+
+        public EventCategory(string name)
+        {
+            Name = name;
+        }
+    }
+}
