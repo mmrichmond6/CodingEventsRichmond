80460207ddbdc74f77e0bfad977251a5f2de56e6
mmrichmond6 <mmrichmond6@gmail.com>
2022-11-16 18:25:36 -0600

    Inlcudes notes from class 11-16

--- CodingEventsDemo/Controllers/EventCategoryController.cs
+++ CodingEventsDemo/Controllers/EventCategoryController.cs
@@ -6,6 +6,7 @@ using CodingEventsDemo.Data;
 using CodingEventsDemo.Models;
 using Microsoft.AspNetCore.Mvc;
 using CodingEventsDemo.ViewModels;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -25,7 +26,28 @@ namespace CodingEventsDemo.Controllers
         public IActionResult Index()
         {
             List<EventCategory> categories = context.Categories.ToList();
+            
+            //Notes from lecture 11-16-22
+            //one to many
+            /*var x = context.Categories
+                .Include(x => x.events)
+                .Where(x => x.Name == "conference")
+                    .Skip(4)
+                    .Take(10)
+                .ToList();*/
+
+            //many to many
+            /*var evt = context.Events
+                .Include(x => x.Tags).ThenInclude(y => y.Tag)
+                .ToList();*/
+
+            /*foreach (var e in x) {
+                Console.WriteLine(e.events.Count);
+            }*/
+            
             return View(categories);
+
+
         }
 
         [HttpGet]
